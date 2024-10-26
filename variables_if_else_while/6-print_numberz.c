832d35c025e15a4836eae9031ea17e5cbef09304
suret1933 <9910@holbertonstudents.com>
2024-10-26 16:16:02 +0000

    numbers 0123456789

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Description: This function prints numbers from 0 to 9,
+ * each followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int num;
