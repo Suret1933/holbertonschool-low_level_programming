969fdd44e8b19d05f964324b5dd335200b626479
suret1933 <9910@holbertonstudents.com>
2024-10-26 23:12:53 +0000

    alphabet2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Küçük harflerle alfabenin 10 kez yazdırılmasını sağlar
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
