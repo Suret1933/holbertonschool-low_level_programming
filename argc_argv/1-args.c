5df92faeb27feb7516a62ca50a60b40e5f50bcc5
suret1933 <9910@holbertonstudents.com>
2024-11-16 19:47:15 +0000

    commit 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-    (void) argv;  /* We are not using argv, so we mark it as unused */
-    _putchar(argc - 1 + '0');  /* Print the number of arguments */
-    _putchar('\n');  /* Newline after printing the number */
-    return (0);
+(void) argv;  /* We are not using argv, so we mark it as unused */
+_putchar(argc - 1 + '0');  /* Print the number of arguments */
+_putchar('\n');  /* Newline after printing the number */
+return (0);
 }
