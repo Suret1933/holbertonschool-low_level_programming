ecb4b081960045d35d6a4152ef83e740a8d0c12d
suret1933 <9910@holbertonstudents.com>
2024-11-16 19:05:22 +0000

    commit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the program
+ *
+ * @argc: Argument count
+ * @argv: Argument vector (array of arguments)
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 (void)argc;  /* argc'yi kullanmadığımızı belirtmek için */
 printf("%s\n", argv[0]);  /* Program adını yazdır */
-return (0);
+return (0);  /* return ifadesi parantezle sarılmalı */
 }
