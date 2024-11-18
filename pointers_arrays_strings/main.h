cc061f18d16025f35e2eba176ec736c0cbc61c38
suret1933 <9910@holbertonstudents.com>
2024-11-18 20:07:04 +0000

    commit.1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,7 +12,8 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
 void reset_to_98(int *n);
-
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 
 
