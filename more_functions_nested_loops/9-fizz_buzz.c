99bef316876f8040f2296c72963106b6ebb9b79b
suret1933 <9910@holbertonstudents.com>
2024-11-18 18:42:33 +0000

    commit 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 1 to 100 with Fizz, Buzz, and FizzBuzz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i != 100) /* Avoid trailing space at the end */
+printf(" ");
+}
+printf("\n");
+return (0);
+}
